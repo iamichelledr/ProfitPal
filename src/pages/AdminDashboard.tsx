9f3b61ee1a34989e3026fe7a038fe4f6fc26202e
iamichelledr <iamichelledr@gmail.com>
2026-04-15 17:38:19 +0800

    Update AdminDashboard.tsx

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -22,8 +22,6 @@ import {
   doc,
   updateDoc,
   serverTimestamp,
-  query,
-  where,
 } from 'firebase/firestore';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
